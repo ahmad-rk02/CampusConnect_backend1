03b9f7e091cb69343175161237c33dc65eb65f1d
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-03-11 11:38:58 +0530

    added

---
+++ dist/src/api/gec-announcement-bar/routes/gec-announcement-bar.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * gec-announcement-bar router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::gec-announcement-bar.gec-announcement-bar');
