461e9379dad7b8ed342e78d979c87e2a4246dc73
ahmad-rk02 <razakhanahmad68@gmail.com>
2025-03-12 02:21:59 +0530

    added

---
+++ dist/src/api/gec-boys-hostel/controllers/gec-boys-hostel.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * gec-boys-hostel controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::gec-boys-hostel.gec-boys-hostel');
